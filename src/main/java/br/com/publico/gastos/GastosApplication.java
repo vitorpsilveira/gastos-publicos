711a6951f6e993829197aeaf01c771fa5e7c665c
Vitor de Paula Silveira - VPA <vitor.paula@iteris.com.br>
2021-02-18 10:30:09 -0300

    criação do projeto

---
+++ src/main/java/br/com/publico/gastos/GastosApplication.java
@@ -0,0 +1,13 @@
+package br.com.publico.gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/publico/gastos/GastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.publico.gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
