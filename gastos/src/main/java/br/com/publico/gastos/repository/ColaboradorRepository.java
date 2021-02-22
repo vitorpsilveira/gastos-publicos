1c4fa9aa2ec7d189b105b0cb8fc248b7a1a94ba7
Victor <victor.alvesf7@gmail.com>
2021-02-22 17:54:09 -0400

    Revert "configurações iniciais"
    This reverts commit 44a4ccacbebce8e999df2f933cb20a626cbf9a9f.

--- gastos/src/main/java/br/com/publico/gastos/repository/ColaboradorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.publico.gastos.repository;
-
-import br.com.publico.gastos.domain.model.Colaborador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
-}
