bfd49c66a33ba6f841f567e3526d63f2f064b893
marcela.fonseca <marcela.fonseca@iteris.com.br>
2021-02-23 17:35:17 -0300

    13-contrato-endpoint-de-salvar-avaliacao - Criação do contrato de requisição da Avaliação

---
+++ gastos/src/main/java/br/com/publico/gastos/controller/request/AvaliacaoRequest.java
@@ -0,0 +1,13 @@
+package br.com.publico.gastos.controller.request;
+
+import java.math.BigDecimal;
+
+public class AvaliacaoRequest {
+
+    private Long colaborador;
+    private Short tipoAvaliacao;
+    private Short resultado;
+    private Short status;
+    private String data;
+    private BigDecimal nota;
+}
