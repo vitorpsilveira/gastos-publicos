33eb2024d002a3a0106f646773ec8de58772816b
marcela.fonseca <marcela.fonseca@iteris.com.br>
2021-02-23 17:28:34 -0300

    12-contrato-endpoint-de-pesquisar-colaborador - Criação do contrato de resposta da pesquisa dos Colaboradores

---
+++ gastos/src/main/java/br/com/publico/gastos/domain/dto/response/ColaboradorResponse.java
@@ -0,0 +1,9 @@
+package br.com.publico.gastos.domain.dto.response;
+
+public class ColaboradorResponse {
+
+    private Long id;
+    private String nome;
+    private String sigla;
+    private Boolean temAvaliacao;
+}
