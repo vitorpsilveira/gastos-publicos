5ce20fd475fd24ded502341bb8ecdfef9e5349f9
Vitor de Paula Silveira - VPA <vitor.paula@iteris.com.br>
2021-02-23 10:26:42 -0300

    Adicionar consulta a sigla de UF

--- gastos/src/main/java/br/com/publico/gastos/domain/dto/mapper/MunicipioMapper.java
+++ gastos/src/main/java/br/com/publico/gastos/domain/dto/mapper/MunicipioMapper.java
@@ -3,9 +3,11 @@ package br.com.publico.gastos.domain.dto.mapper;
 import br.com.publico.gastos.domain.dto.response.ConsultaMunicipioResponse;
 import br.com.publico.gastos.domain.model.Municipio;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MunicipioMapper {
 
+    @Mapping(target = "uf", source = "microrregiao.mesorregiao.UF.sigla")
     ConsultaMunicipioResponse toResponse(Municipio municipio);
 }
