fcd16ca1fe26d7a22abfb172b8fbf972b569bdc4
Victor <victor.alvesf7@gmail.com>
2021-02-23 07:11:35 -0400

    criando uniqueConstraint para a sigla

--- gastos/src/main/java/br/com/publico/gastos/domain/model/Colaborador.java
+++ gastos/src/main/java/br/com/publico/gastos/domain/model/Colaborador.java
@@ -4,16 +4,13 @@ import br.com.publico.gastos.domain.model.base.EntityBase;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "colaborador")
+@Table(name = "colaborador", uniqueConstraints = { @UniqueConstraint(name = "unq_sigla", columnNames = "sigla") })
 public class Colaborador extends EntityBase {
 
     private String nome;
