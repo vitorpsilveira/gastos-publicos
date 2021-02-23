c4f1af4ca652f2400688c7712ae0c7dac6bcda9b
Victor <victor.alvesf7@gmail.com>
2021-02-23 07:34:32 -0400

    adicionando bean validation e concluindo mapeamento das entidades Colaborador e Avaliacao

--- gastos/src/main/java/br/com/publico/gastos/domain/model/Avaliacao.java
+++ gastos/src/main/java/br/com/publico/gastos/domain/model/Avaliacao.java
@@ -3,18 +3,30 @@ package br.com.publico.gastos.domain.model;
 import br.com.publico.gastos.domain.model.base.EntityBase;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "avaliacao")
 public class Avaliacao extends EntityBase {
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "colaborador_id")
+    @JoinColumn(name = "colaborador_id", nullable = false)
+    @NotNull(message = "O colaborador é obrigatório")
     private Colaborador colaborador;
 
     @Enumerated(EnumType.STRING)
-    private TipoAvaliacao avaliacao;
+    @NotNull(message = "O tipo da avalição é obrigatório")
+    private TipoAvaliacao tipoAvaliacao;
 
     @Enumerated(EnumType.STRING)
     private TipoResultado resultado;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDate data;
+
+    private BigDecimal nota;
 }
--- gastos/src/main/java/br/com/publico/gastos/domain/model/Colaborador.java
+++ gastos/src/main/java/br/com/publico/gastos/domain/model/Colaborador.java
@@ -3,8 +3,10 @@ package br.com.publico.gastos.domain.model;
 import br.com.publico.gastos.domain.model.base.EntityBase;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
@@ -13,8 +15,12 @@ import java.util.List;
 @Table(name = "colaborador", uniqueConstraints = { @UniqueConstraint(name = "unq_sigla", columnNames = "sigla") })
 public class Colaborador extends EntityBase {
 
+    @NotBlank(message = "O nome é obrigatório")
+    @Column(length = 100)
     private String nome;
 
+    @NotBlank(message = "A sigla é obrigatório")
+    @Length(max = 3, message = "A sigla deve possuir, no máximo 3 caracteres")
     @Column(length = 3)
     private String sigla;
 
