eb073d1ddf59eae08956f27472177109c62b617b
Victor <victor.alvesf7@gmail.com>
2021-02-24 10:27:01 -0400

    configurando swagger para exibir a swagger ui e adicionando constantes de mapeamento do swagger

---
+++ gastos/src/main/java/br/com/publico/gastos/controller/swagger/SwaggerApiStatusCode.java
@@ -0,0 +1,9 @@
+package br.com.publico.gastos.controller.swagger;
+
+public final class SwaggerApiStatusCode {
+
+    public static final int CODE_200 = 200;
+    public static final int CODE_400 = 400;
+    public static final int CODE_500 = 500;
+
+}
