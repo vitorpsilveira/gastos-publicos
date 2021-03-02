3a0f7a4bb0256759e7f99797ba3d4cce1814da4d
marcela.fonseca <marcela.fonseca@iteris.com.br>
2021-03-02 12:42:17 -0300

    14-endpoint-de-alterar-avaliação-contrato - corrigida a alteração da data de uma avaliação alterando apenas o dia, que antes não permitia alterar

--- gastos/src/main/java/br/com/publico/gastos/controller/AvaliacaoController.java
+++ gastos/src/main/java/br/com/publico/gastos/controller/AvaliacaoController.java
@@ -2,32 +2,34 @@ package br.com.publico.gastos.controller;
 
 
 import br.com.publico.gastos.ExcelHelper;
-import br.com.publico.gastos.domain.dto.response.MensagemResponse;
-import br.com.publico.gastos.domain.model.Avaliacao;
-import br.com.publico.gastos.services.AvaliacaoService;
-import br.com.publico.gastos.services.ValidacoesService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import br.com.publico.gastos.controller.exceptionhandler.Problema;
 import br.com.publico.gastos.controller.request.AvaliacaoRequest;
 import br.com.publico.gastos.controller.request.AvaliacaoUpdateRequest;
 import br.com.publico.gastos.controller.swagger.SwaggerApiMessage;
 import br.com.publico.gastos.controller.swagger.SwaggerApiStatusCode;
 import br.com.publico.gastos.domain.dto.response.AvaliacaoResponse;
+import br.com.publico.gastos.domain.dto.response.MensagemResponse;
+import br.com.publico.gastos.domain.model.Avaliacao;
+import br.com.publico.gastos.services.AvaliacaoService;
+import br.com.publico.gastos.services.ValidacoesService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -82,7 +84,7 @@ public class AvaliacaoController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     @ApiOperation(value = "Editar avaliação")
     @ApiResponses(value = { @ApiResponse(code = SwaggerApiStatusCode.CODE_200, message = "Editar e retornar status 200"),
             @ApiResponse(code = SwaggerApiStatusCode.CODE_400, message = SwaggerApiMessage.REQUISICAO_INVALIDA, response = Problema.class)})
--- gastos/src/main/java/br/com/publico/gastos/repository/AvaliacaoRepository.java
+++ gastos/src/main/java/br/com/publico/gastos/repository/AvaliacaoRepository.java
@@ -2,13 +2,22 @@ package br.com.publico.gastos.repository;
 
 import br.com.publico.gastos.domain.model.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
 
-    List<Avaliacao> findByColaboradorIdAndDataBetween(Long idColaborador, LocalDate dataInicial, LocalDate dataFinal);
+    @Query("SELECT a " +
+           "FROM Avaliacao a " +
+           "WHERE a.colaborador.id = :idColaborador " +
+           "AND a.data BETWEEN :dataInicial AND :dataFinal " +
+           "AND a.id <> :idAvaliacao ")
+    List<Avaliacao> findByColaboradorIdAndDataBetween(@Param("idColaborador") Long idColaborador, @Param("dataInicial") LocalDate dataInicial, @Param("dataFinal") LocalDate dataFinal, @Param("idAvaliacao") Long idAvaliacao);
+
+    List<Avaliacao> findByColaboradorIdAndDataBetween(@Param("idColaborador") Long idColaborador, @Param("dataInicial") LocalDate dataInicial, @Param("dataFinal") LocalDate dataFinal);
 
     List<Avaliacao> findByColaboradorNomeAndColaboradorSiglaAndDataBetween(String nomeColaborador, String siglaColaborador, LocalDate dataInicial, LocalDate dataFinal);
 
